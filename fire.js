b3d4376f6d6cd43ad2b62c97a16bd8549ad75510
diegoguzman339 <82859636+diegoguzman339@users.noreply.github.com>
2023-05-10 18:52:01 -0600

    Update fire.js

--- fire.js
+++ fire.js
@@ -2,14 +2,14 @@ var firebase = require('firebase')
 // datos de firebase
 var config = {
 
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyBh2i1CCIaz6uY6uz4EVao69HiskvxrSZk",
+  authDomain: "monitor-9a16f.firebaseapp.com",
+  databaseURL: "https://monitor-9a16f-default-rtdb.firebaseio.com",
+  projectId: "monitor-9a16f",
+  storageBucket: "monitor-9a16f.appspot.com",
+  messagingSenderId: "864280604013",
+  appId: "1:864280604013:web:080f367dffe33953edfe64",
+  measurementId: "G-23M3TSBJ52"
 };
 
 const fire = firebase.initializeApp(config);
